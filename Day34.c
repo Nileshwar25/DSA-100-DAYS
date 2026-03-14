59945f89a3b5397c047be86a4bfadc9afbbc96c5
Nileshwar Pratap Gahloth <gahlothnileshwar@gmail.com>
2026-03-14 16:43:55 +0530

    New commit

---
+++ Day34.c
@@ -0,0 +1,73 @@
+/*Problem: Evaluate Postfix Expression - Implement using linked list with dynamic memory allocation.
+
+Input:
+- Postfix expression with operands and operators
+
+Output:
+- Print the integer result
+
+Example:
+Input:
+2 3 1 * + 9 -
+
+Output:
+-4
+
+Explanation:
+Use stack to store operands, apply operators by popping operands, push result back. Final stack top is result.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+void push(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = top;
+    top = newNode;
+}
+
+int pop() {
+    if (top == NULL) {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    struct Node* temp = top;
+    int value = temp->data;
+    top = top->next;
+    free(temp);
+    return value;
+}
+
+int main() {
+    int a, b;
+    char token;
+
+    while (scanf(" %c", &token) != EOF) {
+
+        if (token >= '0' && token <= '9') {
+            push(token - '0');
+        }
+
+        else {
+            b = pop();
+            a = pop();
+
+            switch(token) {
+                case '+': push(a + b); break;
+                case '-': push(a - b); break;
+                case '*': push(a * b); break;
+                case '/': push(a / b); break;
+            }
+        }
+    }
+
+    printf("%d", pop());
+
+    return 0;
+}
